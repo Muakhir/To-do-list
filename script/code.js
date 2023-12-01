8a9818b139e02073f3bd9345344938bccdcfc802
Muakhir <almuakhirjones@gmail.com>
2023-12-01 12:03:21 +0200

    mistake

--- script/code.js
+++ script/code.js
@@ -1,35 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let inputElement = document.querySelector('[data-entry]')
-    let submitButton = document.querySelector('[data-submit]')
-    let sortButton = document.querySelector('[data-sort]')
+let inputElement = document.querySelector('[data-entry]')
+let submitButton = document.querySelector('[data-submit]')
+let sortButton = document.querySelector('[data-sort]')
+let tasks = [""]
 
     inputElement.addEventListener('input', capitalizeFirstLetter)
     submitButton.addEventListener('click', submitTask)
-    sortButton.addEventListener('click', sortTasks)
-
+    sortButton.addEventListener('click', function () {
+    sortTasks(sortButton.dataset.order);
+    })
+        
 function capitalizeFirstLetter() {
     let inputValue = inputElement.value
-        if (inputValue.length > 0) {
+    if (inputValue.length > 0) {
         let capitalizedText = inputValue.charAt(0).toUpperCase() + inputValue.slice(1)
-            inputElement.value = capitalizedText;
+        inputElement.value = capitalizedText;
         }
     }
 function submitTask() {
-    let task = inputElement.value
-    if (task.trim() !== '') {
-        let listItem = document.createElement('li')
-            listItem.textContent = task
-        let taskList = document.getElementById('taskList');
-        if (taskList) {
-                taskList.appendChild(listItem);
-            }
-            inputElement.value = '';
+        let task = inputElement.value
+        if (task.trim() !== '') {
+            tasks.push(task)
+            renderTasks()
+            inputElement.value = ''
         }
     }
-    
+function sortTasks(order) {
+    if (order === 'asc') {
+            tasks.sort()
+        } 
+    else if (order === 'desc') {
+            tasks.sort().reverse()
+        }
+        renderTasks()
+    }
 
-function sortTasks() {
+function renderTasks() {
+    let taskList = document.getElementById('taskList')
+    if (taskList) {
+            taskList.innerHTML = ''
 
-    alert('Sorting tasks...')
+    for (let i = 0; i < tasks.length; i++) {
+            let listItem = document.createElement('li')
+                listItem.textContent = tasks[i]
+                taskList.appendChild(listItem)
+            }
+
+        }
     }
 })
+
